95ade36d0c625faa9f0c1711d53c6fe62b3a7c5e
Adrian L Lange <git@p3lim.net>
2022-11-06 13:55:47 +0100

    Add button creation method
    This makes it easier to deal with key directions

---
+++ modules/widgets.lua
@@ -0,0 +1,26 @@
+local _, addon = ...
+
+local KEY_DIRECTION_CVAR = 'ActionButtonUseKeyDown'
+
+local function updateKeyDirection(self)
+	if C_CVar.GetCVarBool(KEY_DIRECTION_CVAR) then
+		self:RegisterForClicks('AnyDown')
+	else
+		self:RegisterForClicks('AnyUp')
+	end
+end
+
+local function onCVarUpdate(self, cvar)
+	if not cvar or cvar == KEY_DIRECTION_CVAR then
+		addon:Defer(updateKeyDirection, self)
+	end
+end
+
+function addon:CreateButton(...)
+	local button = CreateFrame(...)
+	Mixin(button, addon.eventMixin)
+
+	button:RegisterEvent('CVAR_UPDATE', updateKeyDirection)
+
+	return button
+end
