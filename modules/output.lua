ff69e48a82ea44c3cef41932cb80244951cb6c5d
Adrian L Lange <git@p3lim.net>
2022-11-16 20:15:57 +0100

    Replace string.join

--- modules/output.lua
+++ modules/output.lua
@@ -1,7 +1,18 @@
 local addonName, addon = ...
 
 function addon:Print(...)
-	DEFAULT_CHAT_FRAME:AddMessage('|cff33ff99' .. addonName .. '|r: ' .. string.join(' ', ...))
+	-- can't use string join, it fails on nil values
+	local msg = ''
+	for index = 1, select('#', ...) do
+		local arg = select(index, ...)
+		if arg == nil then
+			arg = 'nil'
+		end
+
+		msg = msg .. arg .. ' '
+	end
+
+	DEFAULT_CHAT_FRAME:AddMessage('|cff33ff99' .. addonName .. '|r: ' .. msg:trim())
 end
 
 function addon:Printf(fmt, ...)
