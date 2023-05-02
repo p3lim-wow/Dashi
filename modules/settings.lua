d4a54aed5b18540167566262456f3ec82270132f
Adrian L Lange <git@p3lim.net>
2023-05-02 18:19:39 +0200

    Add settings shorthands

---
+++ modules/settings.lua
@@ -0,0 +1,23 @@
+local _, addon = ...
+
+function addon:OpenSettings(name)
+	if addon:IsRetail() then
+		Settings.OpenToCategory(name)
+	else
+		InterfaceOptionsFrame_OpenToCategory(name)
+		InterfaceOptionsFrame_OpenToCategory(name) -- load twice due to an old bug
+	end
+end
+
+function addon:HookSettings(callback)
+	if addon:IsRetail() then
+		SettingsPanel:HookScript('OnShow', callback)
+	else
+		InterfaceOptionsFrameAddOns:HookScript('OnShow', function(frame)
+			callback(frame)
+
+			-- we load too late, so we have to manually refresh the list
+			InterfaceAddOnsList_Update()
+		end)
+	end
+end
