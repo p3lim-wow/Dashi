3639e9847a3a710a49b25196f29dc3279278935d
Adrian L Lange <git@p3lim.net>
2024-11-01 19:19:31 +0100

    Add utility to check for build number

--- modules/misc.lua
+++ modules/misc.lua
@@ -9,7 +9,7 @@ local _, namespace = ...
 --]]
 
 -- game version API
-local _, _, _, interfaceVersion = GetBuildInfo()
+local _, buildVersion, _, interfaceVersion = GetBuildInfo()
 --[[ namespace:IsRetail()
 Checks if the current client is running the "retail" version.
 --]]
@@ -29,6 +29,18 @@ Checks if the current client is running the "classic" version.
 --]]
 function addon:IsClassic()
 	return not addon:IsRetail() and not addon:IsClassicEra()
+end
+
+--[[ namespace:HasBuild(_buildNumber_[, _interfaceVersion_])
+Checks if the current client is running a build equal to or newer than the specified.  
+Optionally also check against the interface version.
+--]]
+function addon:HasBuild(build, interface)
+	if interface and interfaceVersion < interface then
+		return
+	end
+
+	return tonumber(buildVersion) >= build
 end
 
 --[[ namespace:ArgCheck(arg, argIndex, type[, type...])
