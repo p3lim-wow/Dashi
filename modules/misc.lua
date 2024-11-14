90052cdc7afbb9f0d777156716a80122bf6a8e71
Adrian L Lange <git@p3lim.net>
2024-11-14 19:46:13 +0100

    Move library methods to its own file

--- modules/misc.lua
+++ modules/misc.lua
@@ -181,25 +181,3 @@ function addon:IsAddOnEnabled(name)
 	local _, _, _, loadable = C_AddOns.GetAddOnInfo(name)
 	return not not loadable -- will be false if the addon is missing or disabled
 end
-
---[[ namespace:tsize(_table_)
-Returns the number of entries in the `table`.  
-Works for associative tables as opposed to `#table`.
---]]
-function addon:tsize(tbl)
-	-- would really like Lua 5.2 for this
-	local size = 0
-	if tbl then
-		for _ in next, tbl do
-			size = size + 1
-		end
-	end
-	return size
-end
-
---[[ namespace:startswith(_str_, _contents_)
-Checks if the first string starts with the 2nd string.
---]]
-function addon:startswith(str, contents)
-	return str:sub(1, contents:len()) == contents
-end
