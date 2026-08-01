0cea486c58dbc109abbfc0b06f8be6fbcb9d289c
Adrian L Lange <git@p3lim.net>
2026-08-01 12:44:50 +0200

    defer: Add some event defer wrapping

--- modules/defer.lua
+++ modules/defer.lua
@@ -69,3 +69,46 @@ function addon:DeferMethod(object, method, ...)
 		object[method](object, ...)
 	end
 end
+
+local function deferEventCallback(callback, ...)
+	addon:Defer(callback, ...)
+	return true -- always unregister, don't want this event to trigger multiple defers
+end
+
+--[[ namespace:DeferEvent(_event_, _callback_[, _..._]) ![](https://img.shields.io/badge/function-blue)
+Defers a function `callback` (with optional arguments) until after combat ends and `event` has triggered.
+Triggers when `event` triggers if not in combat.
+--]]
+function addon:DeferEvent(event, callback, ...)
+	addon:ArgCheck(event, 1, 'string')
+	addon:ArgCheck(callback, 2, 'function')
+	addon:RegisterEvent(event, GenerateClosure(deferEventCallback, callback, ...))
+end
+
+--[[ namespace:DeferUnitEvent(_event_, _unit_[, _unitN,..._], _callback_[, _..._]) ![](https://img.shields.io/badge/function-blue)
+Defers a function `callback` (with optional arguments) until after combat ends and `event` has triggered for _unit_(s).
+Triggers when `event` triggers if not in combat.
+--]]
+function addon:DeferUnitEvent(event, ...)
+	local args = {} -- we need to pack the args, sadly Lua 5.1 doesn't have table.pack
+	local callback, unitindexlast, argindexfirst, lastindex
+	for i = 1, select('#', ...) do
+		local arg = select(i, ...)
+		if not callback and type(arg) == 'function' then
+			-- assume it's the callback
+			callback = arg
+
+			unitindexlast = i - 1
+			argindexfirst = i + 1
+		end
+
+		args[i] = arg -- pack
+		lastindex = i -- need to supply `last` to unpack since it'd stop unpacking when hitting nil
+	end
+
+	assert(callback, 'no callback provided')
+	assert(unitindexlast > 0, 'no units provided')
+
+	local closure = GenerateClosure(deferEventCallback, callback, unpack(args, argindexfirst, lastindex))
+	addon:RegisterUnitEvent(event, unpack(args, 1, unitindexlast), closure)
+end
