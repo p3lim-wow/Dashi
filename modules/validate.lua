28291ed83bcbd868534f81772c6855f3d908cd91
Adrian L Lange <git@p3lim.net>
2026-08-16 13:36:10 +0200

    validate: Use varargs to format message

--- modules/validate.lua
+++ modules/validate.lua
@@ -1,10 +1,10 @@
 local _, addon = ...
 
---[[ namespace:ArgCheck(arg, argIndex, types[, message]) ![](https://img.shields.io/badge/function-blue)
+--[[ namespace:ArgCheck(arg, argIndex, types[, message][, ...]) ![](https://img.shields.io/badge/function-blue)
 Checks if the argument `arg` at position `argIndex` is of `types`.  
 Types can be one or multiple types, separated by |.
 --]]
-function addon:ArgCheck(arg, argIndex, expected, message)
+function addon:ArgCheck(arg, argIndex, expected, message, ...)
 	assert(type(argIndex) == 'number', "Bad argument #2 to 'ArgCheck' (number expected, got " .. type(argIndex) .. ')')
 	assert(type(expected) == 'string', "Bad argument #3 to 'ArgCheck' (string expected, got " .. type(expected) .. ')')
 
@@ -16,23 +16,23 @@ function addon:ArgCheck(arg, argIndex, expected, message)
 
 	local name = debugstack(2, 2, 0):match(": in function [`<](.-)['>]")
 	if message then
-		error(string.format("Bad argument #%d to '%s' (%s)", argIndex, name, message), 3)
+		error(string.format("Bad argument #%d to '%s' (%s)", argIndex, name, message:format(...)), 3)
 	end
 
 	local types = expected:gsub('|', '/')
 	error(string.format("Bad argument #%d to '%s' (%s expected, got %s)", argIndex, name, types, type(arg)), 3)
 end
 
---[[ namespace:ArgAssert(state, argIndex, message) ![](https://img.shields.io/badge/function-blue)
+--[[ namespace:ArgAssert(state, argIndex, message[, ...]) ![](https://img.shields.io/badge/function-blue)
 If state is false; throw an error for arg at index `argIndex` with a `message`.
 --]]
-function addon:ArgAssert(state, argIndex, message)
+function addon:ArgAssert(state, argIndex, message, ...)
 	assert(type(state) == 'boolean', "Bad argument #1 to 'ArgAssert' (boolean expected, got " .. type(state) .. ')')
 	assert(type(argIndex) == 'number', "Bad argument #2 to 'ArgAssert' (number expected, got " .. type(argIndex) .. ')')
 	assert(type(message) == 'string', "Bad argument #3 to 'ArgAssert' (string expected, got " .. type(message) .. ')')
 
 	if not state then
 		local name = debugstack(2, 2, 0):match(": in function [`<](.-)['>]")
-		error(string.format("Bad argument #%d to '%s' (%s)", argIndex, name, message), 3)
+		error(string.format("Bad argument #%d to '%s' (%s)", argIndex, name, message:format(...)), 3)
 	end
 end
