5ea321d90f02d9970c568ed412ca8e428441c040
Adrian L Lange <git@p3lim.net>
2026-08-16 13:07:54 +0200

    defer: The argcheck is enough

--- modules/defer.lua
+++ modules/defer.lua
@@ -34,10 +34,6 @@ function addon:Defer(callback, ...)
 		callback = _G[callback]
 	end
 
-	if not callback then
-		error('callback is nil') -- TODO: pretty this up
-	end
-
 	addon:ArgCheck(callback, 1, 'function')
 
 	if InCombatLockdown() then
