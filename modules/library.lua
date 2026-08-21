70097a1921113f2f3a750734b63a3f4416d07930
Adrian L Lange <git@p3lim.net>
2026-08-21 15:44:44 +0200

    library: Document all the table methods

--- modules/library.lua
+++ modules/library.lua
@@ -48,12 +48,15 @@ end
 --[[ namespace:T([_tbl_]) ![](https://img.shields.io/badge/function-blue)
 Returns the table _`tbl`_ with meta methods. If _`tbl`_ is not provided a new table is created.
 
-Included are all meta methods from the `table` library, as well as a few extra handy methods:
+Included are all meta methods from the [`table` library](https://warcraft.wiki.gg/wiki/Lua_functions#Table_library), as well as a few extra handy methods:
 
 - `tbl:size()` returns the number of entries in the table
 - `tbl:contains(value)` returns `true` if the table contains the given `value`, otherwise `false`
 - `tbl:merge(t)` merges (and returns) the table with the supplied table `t`
     - can also be used by using an addition arithmetic metamethod
+- `tbl:random()` returns a random value from the table
+- `tbl:copy(shallow)` creates and returns a copy of the table
+- `tbl:removeValue(value)` removes an entry from the table that matches the value
 
 It's also possible to add extra meta methods by supplying mixins through the variable argument.
 
